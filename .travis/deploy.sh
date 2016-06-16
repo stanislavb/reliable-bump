477a67c9eb1e36f07c1ae2b5849c86ffa0109530
Stanislav Blokhin <stanislav.blokhin@tomologic.com>
2016-06-16 23:07:53 +0200

    Travis checkout master before deploy
    Also check for infinite git push loop when Travis reacts to its own push
    Additionally, do not accidently check in deploy SSH key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,6 +5,7 @@
 # travis login
 # ssh-keygen -t rsa -b 4096 -f ./.travis/deploy_key
 # travis encrypt-file ./.travis/deploy_key ./.travis/deploy_key.enc --add
+# echo ".travis/deploy_key" >> .gitignore
 # rm ./.travis/deploy_key
 # In Github, add deploy_key.pub contents as a deploy key with push permissions
 
@@ -14,14 +15,17 @@ if [ "${TRAVIS_BRANCH}" != "master" ]; then
     exit 1
 fi
 
-# Travis works in "detached head" mode, so we need to fix it first.
-git fetch --all
-git reset --hard deploy/master
-git status
+# If Travis reacts to push events, our git push in this very script
+# will trigger another Travis build. To avoid an infinite bump loop,
+# check if HEAD already is tagged with a semver tag.
+git tag --contains HEAD | grep -q "[0-9]*\.[0-9]*\.[0-9]*"
+if [[ $? -eq 0 ]]; then
+  echo "HEAD is already tagged with a version. Doing nothing."
+else
+  # Use our own code
+  ./bin/bump.js
+  git status
 
-# Use our own code
-./bin/bump.js
-git status
-
-# Push to remote
-git push --verbose --follow-tags deploy master
+  # Push to remote
+  git push --verbose --follow-tags deploy master
+fi
